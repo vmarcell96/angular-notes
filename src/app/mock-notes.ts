06e97fc890c2d31c205d65f14ee344c1591df521
vmarcell96 <vajdam96@gmail.com>
2022-12-01 10:38:39 +0100

    mock notes database

---
+++ src/app/mock-notes.ts
@@ -0,0 +1,10 @@
+import { Note } from "./note";
+
+export const NOTES: Note[] = [
+    { id: 7, title: "Note 7", content: "Note 7 content", tags: ["programming", "angular", "typescript"] },
+    { id: 8, title: "Note 8", content: "Note 8 content", tags: ["programming", "angular", "typescript"] },
+    { id: 9, title: "Note 9", content: "Note 9 content", tags: ["programming", "angular", "typescript"] },
+    { id: 10, title: "Note 10", content: "Note 10 content", tags: ["programming", "angular", "typescript"] },
+    { id: 11, title: "Note 11", content: "Note 11 content", tags: ["programming", "angular", "typescript"] },
+    { id: 12, title: "Note 12", content: "Note 12 content", tags: ["programming", "angular", "typescript"] },
+]
